bce5ea8a6e57d15d77397fb55296347826fadb64
Mr.J <129696486+Mr-J486@users.noreply.github.com>
2024-12-12 22:58:10 +0200

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -1,16 +1,13 @@
 import os
+import shutil
+from traceback import print_tb
+
+
 def convert_to_seconds(time_str):
     hours, minutes, seconds = time_str.split(':')
     return int(hours) * 3600 + int(minutes) * 60 + int(seconds)
-
-
-# Initialize an empty dictionary to store the data
-file_data = {}
-
-# Open the file
 def readfile(folderdir):
     target_path = os.path.abspath(folderdir)
-    print(target_path)
     with open(target_path, 'r') as file:
         num_entries = int(file.readline().strip())
 
@@ -19,13 +16,15 @@ def readfile(folderdir):
 
             filename, time_str = line.split()
 
-            key = int(filename.split('.')[0])
+            #key = int(filename.split('.')[0])
+            key = filename
 
             value = convert_to_seconds(time_str)
 
             file_data[key] = value
 
-
-
+    
+    return file_data
+file_data = {}
 readfile(r"..\testcases\Sample Tests\Sample 1\INPUT\AudiosInfo.txt")
 print(file_data)
