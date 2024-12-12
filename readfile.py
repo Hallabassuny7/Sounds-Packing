892b7833ba8f0cf6e563ae401d59eb7d9e1d98bf
Mr.J <129696486+Mr-J486@users.noreply.github.com>
2024-12-12 17:49:54 +0200

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -1,3 +1,4 @@
+import os
 def convert_to_seconds(time_str):
     hours, minutes, seconds = time_str.split(':')
     return int(hours) * 3600 + int(minutes) * 60 + int(seconds)
@@ -7,19 +8,24 @@ def convert_to_seconds(time_str):
 file_data = {}
 
 # Open the file
-with open(r"D:\Faculty\5th_Semester\Algo\Project\testcases\Complete1\AudiosInfo.txt", 'r') as file:
-    num_entries = int(file.readline().strip())
+def readfile(folderdir):
+    target_path = os.path.abspath(folderdir)
+    print(target_path)
+    with open(target_path, 'r') as file:
+        num_entries = int(file.readline().strip())
 
-    for i in range(num_entries):
-        line = file.readline().strip()
+        for i in range(num_entries):
+            line = file.readline().strip()
 
-        filename, time_str = line.split()
+            filename, time_str = line.split()
 
-        key = int(filename.split('.')[0])
+            key = int(filename.split('.')[0])
 
-        value = convert_to_seconds(time_str)
+            value = convert_to_seconds(time_str)
 
-        file_data[key] = value
+            file_data[key] = value
 
 
 
+readfile(r"..\testcases\Sample Tests\Sample 1\INPUT\AudiosInfo.txt")
+print(file_data)
