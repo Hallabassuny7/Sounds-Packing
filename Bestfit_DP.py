25b234ab125303c35de304f3b20870a04ac95872
Darkenergy <fatma-7@github.com>
2024-12-14 21:19:30 +0200

    tuples modify

--- Bestfit_DP.py
+++ Bestfit_DP.py
@@ -1,30 +1,29 @@
-
-def best_fit_DP(file_sizes, folder_capacity):#The bottom-up approach
+def best_fit_DP(file_sizes, folder_capacity):  # The bottom-up approach
    
     files = list(file_sizes.items())
     n = len(files)
     
+    # Sort files in descending order by size
     files.sort(key=lambda x: x[1], reverse=True)  
 
+    # dp[i][j] tracks the minimum number of folders needed for the first `i` files and `j` folders
     dp = [[float('inf')] * (n + 1) for _ in range(n + 1)]
     dp[0][0] = 0  # Base case: no files, no folders
 
     for i in range(1, n + 1):  
         for j in range(1, n + 1): 
-            
             if files[i - 1][1] <= folder_capacity:
-                dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1) #take (Adding the File to the Current Folder)
+                dp[i][j] = min(dp[i][j], dp[i - 1][j] + 1)  # Add file to the current folder
+            
+            dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1)  # Create a new folder
 
-        
-            dp[i][j] = min(dp[i][j], dp[i - 1][j - 1] + 1)  #leave (Moving the File to a New Folder)
-
-    #  backtracking the solution (Folder assignments)
+    # Backtracking to determine the folder assignments
     folders = []
     folder_index = 0
     for i in range(1, n + 1):
-        folder = {}
+        folder = []
         for j in range(folder_index, n + 1):
-            folder[files[i - 1][0]] = files[i - 1][1]
+            folder.append((files[i - 1][0], files[i - 1][1]))  # Add file as a tuple
             folder_index = j + 1
         folders.append(folder)
 
--- Bestfit_greedy.py
+++ Bestfit_greedy.py
@@ -1,28 +1,27 @@
 import heapq 
 
 def best_fit_greedy(file_sizes, folder_capacity): 
-    
-    # Sort files in dec order 
+    # Sort files in descending order by size
     sorted_files = sorted(file_sizes.items(), key=lambda x: x[1], reverse=True)
 
-    # tracking the remaining capacities of the folders
+    # Tracking the remaining capacities of the folders
     heap = []
     # List to store the actual folders
     folders = []
     
     for file_name, size in sorted_files:
-     # (Greedy choice):Trying to fit the file into an existing folder
+        # (Greedy choice): Try to fit the file into an existing folder
         if heap and heap[0][0] >= size:
-            # Subproblem:Finding the best existing folde
+            # Subproblem: Find the best existing folder
             remaining_capacity, folder_index = heapq.heappop(heap)
             remaining_capacity -= size
-            folders[folder_index][file_name] = size
+            folders[folder_index].append((file_name, size))  # Append file as a tuple
             heapq.heappush(heap, (remaining_capacity, folder_index))
         else:
-            #(Greedy choice):Create a new folder if no existing folder can fit the file
+            # (Greedy choice): Create a new folder if no existing folder can fit the file
             new_remaining = folder_capacity - size
             folder_index = len(folders)
-            folders.append({file_name: size})
+            folders.append([(file_name, size)])  # New folder as a list of tuples
             heapq.heappush(heap, (new_remaining, folder_index))
 
     return folders
